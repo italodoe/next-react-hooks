1765e1216df8e5d5858e78d244605c2e1c232a0a
Italo Castillo <italogitdoe@gmail.com>
2024-06-15 03:53:31 +0200

    build client side page

--- app/client-user-list/page.tsx
+++ app/client-user-list/page.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+"use client";
+
+import UserComponent from "@/components/UserComponent";
+import { User, loadUsers } from "@/lib/users";
+import React, { useEffect, useState } from "react";
 
 export default function Page() {
-  return (
-  <main>
+  const [userList, setUserList] = useState<User[] | null>(null);
 
-  </main>
+  useEffect(() => {
+    // loadUsers().then((user) => setUserList(user));
+    loadUsers().then(setUserList);
+  }, []); //WITH array empty calling only one
+
+  if (userList === null) return <div>Loading...</div>;
+
+  return (
+    <main>
+      <h1 >
+        User list
+      </h1>
+      <div className="flex flex-col gap-4 ">
+        {userList.map((user) => (
+          <UserComponent key={user.email} userProp={user}></UserComponent>
+        ))}
+      </div>
+    </main>
   );
 }
